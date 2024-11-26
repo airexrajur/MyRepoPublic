58586139f19bd7ee93b5c4225e09a69eb937f8df
AirExRaju <airexrajur@gmail.com>
2024-11-26 15:51:02 +0530

    Create StringExtensions.cs

---
+++ ConsoleApp1/StringExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class StringExtensions
+    {
+        public static string IsNullOrEmpty(string str) 
+        {
+            if (str != null && !string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+            else { 
+                return string.Empty;
+            }
+        }
+
+        public static object IsNullOrEmpty(object str)
+        {
+            if (str != null )
+            {
+                return str;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+
+    }
+}
